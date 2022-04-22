5b38b289ecec25e7cd3725ac3d22b69a2d28adb9
Sam Samskies <samsamskies@gmail.com>
2022-04-22 15:50:32 -0500

    setup dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import theme from "styles/theme";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <CSSReset />
       <Component {...pageProps} />
     </ChakraProvider>
