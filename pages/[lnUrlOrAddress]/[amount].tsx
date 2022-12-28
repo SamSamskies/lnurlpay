8bfa204a848ddcb2fbf4a8025ece5b43734caad7
Sam Samskies <samsamskies@gmail.com>
2022-12-28 09:25:20 -1000

    remove unnecessary invoice validations
    invoice validations are now done in lnurl-pay lib.

--- pages/[lnUrlOrAddress]/[amount].tsx
+++ pages/[lnUrlOrAddress]/[amount].tsx
@@ -2,33 +2,15 @@ import { GetServerSideProps } from "next";
 import { requestInvoice } from "lnurl-pay";
 import InvoicePage from "components/InvoicePage";
 import type { Satoshis } from "lnurl-pay/dist/types/types";
-import lightningPayReq from "bolt11";
-
-const verifyDescriptionHash = async (metadataHash: string, invoice: string) => {
-  return (
-    metadataHash ===
-    lightningPayReq.decode(invoice).tagsObject.purpose_commit_hash
-  );
-};
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   try {
-    const { invoice, params } = await requestInvoice({
+    const { invoice } = await requestInvoice({
       lnUrlOrAddress: query.lnUrlOrAddress as string,
       tokens: Number(query.amount) as Satoshis,
       comment: query.comment as string,
+      validateInvoice: true,
     });
-
-    const isValidAmount =
-      lightningPayReq.decode(invoice).satoshis === Number(query.amount);
-    const isValidDescriptionHash = await verifyDescriptionHash(
-      params.metadataHash,
-      invoice
-    );
-
-    if (!isValidAmount || !isValidDescriptionHash) {
-      throw new Error("Error generating invoice :(");
-    }
 
     return {
       props: { invoice },
