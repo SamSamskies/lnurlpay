9b1010e3fddd28bdadee84bc6ba5ac9eddb9e7bb
Sam Samskies <samsamskies@gmail.com>
2023-01-07 20:41:53 -0600

    add error handling

--- pages/api/invoice.ts
+++ pages/api/invoice.ts
@@ -8,14 +8,29 @@ export default async function handler(
 ) {
   switch (req.method) {
     case "GET":
-      const { invoice } = await requestInvoice({
-        lnUrlOrAddress: req.query.lnUrlOrAddress as string,
-        tokens: Number(req.query.amount) as Satoshis,
-        comment: req.query.comment as string,
-        validateInvoice: true,
-      });
+      try {
+        const { invoice } = await requestInvoice({
+          lnUrlOrAddress: req.query.lnUrlOrAddress as string,
+          tokens: Number(req.query.amount) as Satoshis,
+          comment: req.query.comment as string,
+          validateInvoice: true,
+        });
 
-      res.status(200).json(invoice);
+        res.status(200).json(invoice);
+      } catch (error) {
+        if (
+          error instanceof Error &&
+          ["Invalid lnUrlOrAddress", "Invalid amount"].includes(error.message)
+        ) {
+          res.status(400).end(error.message);
+        } else {
+          res
+            .status(500)
+            .end(
+              error instanceof Error ? error.message : "Something went wrong :("
+            );
+        }
+      }
       break;
     default:
       res.setHeader("Allow", ["GET"]);
