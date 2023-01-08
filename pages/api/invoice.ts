bc2196ec7d16c324d81c57045d9ad111c449e122
Sam Samskies <samsamskies@gmail.com>
2023-01-07 18:37:48 -0600

    add cors for invoice endpoint

--- pages/api/invoice.ts
+++ pages/api/invoice.ts
@@ -2,10 +2,27 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { requestInvoice } from "lnurl-pay";
 import type { Satoshis } from "lnurl-pay/dist/types/types";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+const allowCors =
+  (fn: (req: NextApiRequest, res: NextApiResponse) => Promise<void>) =>
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
+    }
+    return await fn(req, res);
+  };
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case "POST":
       const { invoice } = await requestInvoice({
@@ -22,3 +39,5 @@ export default async function handler(
       res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
+
+module.exports = allowCors(handler);
