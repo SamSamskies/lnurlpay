f029f330ec570026861d4a3d29ecd292aa599f73
Sam Samskies <samsamskies@gmail.com>
2023-01-13 14:46:42 -0600

    pivot back to @vercel/og
    other way doesn't seem to work with edge functions.

---
+++ pages/api/og.tsx
@@ -0,0 +1,66 @@
+import { ImageResponse } from "@vercel/og";
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const lnUrlOrAddress = searchParams.get("lnUrlOrAddress");
+    const amount = searchParams.get("amount");
+    const normalizeAmount = (amount?: number) =>
+      amount === 1 ? `${amount} sat` : `${amount} sats`;
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: "#1A202C",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            color: "white",
+            fontFamily: "system-ui, Helvetica, Arial, sans-serif",
+          }}
+        >
+          {lnUrlOrAddress && (
+            <div
+              style={{
+                fontSize: 48,
+              }}
+            >
+              {lnUrlOrAddress}
+            </div>
+          )}
+          {amount && (
+            <div
+              style={{
+                fontSize: 36,
+                marginTop: 24,
+              }}
+            >
+              {`Tip amount: ${normalizeAmount(Number(amount))}`}
+            </div>
+          )}
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
