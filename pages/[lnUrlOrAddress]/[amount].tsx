7292edcda0805c9a710d9e411878d4a177d5e484
Sam Samskies <samsamskies@gmail.com>
2023-11-30 15:32:48 -0600

    validate amount, but not description hash
    description hash validation is being removed from the spec https://github.com/lnurl/luds/pull/234

--- pages/[lnUrlOrAddress]/[amount].tsx
+++ pages/[lnUrlOrAddress]/[amount].tsx
@@ -5,12 +5,15 @@ import type { Satoshis } from "lnurl-pay/dist/types/types";
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   try {
-    const { invoice } = await requestInvoice({
+    const { invoice, hasValidAmount } = await requestInvoice({
       lnUrlOrAddress: query.lnUrlOrAddress as string,
       tokens: Number(query.amount) as Satoshis,
       comment: query.comment as string,
-      validateInvoice: true,
     });
+
+    if (!hasValidAmount) {
+      return { props: { error: "Something went wrong with generating the invoice." } };
+    }
 
     return {
       props: {
